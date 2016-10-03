2926e2714393eab1cb8ffee3e3f460db3015005e
Kai Cataldo <kaicataldo@gmail.com>
2016-10-03 12:44:17 -0400

    env-specific plugins

--- index.js
+++ index.js
@@ -1,25 +1,27 @@
+var env = process.env.BABEL_ENV || process.env.NODE_ENV;
+
 module.exports = {
   presets: [
-    require("babel-preset-es2015"),
+    require("babel-preset-latest"),
     require("babel-preset-react"),
     require("babel-preset-stage-3")
   ],
   plugins: [
-    require("babel-plugin-add-module-exports"),
-    require("babel-plugin-syntax-trailing-function-commas")
-  ],
-  "env": {
-    "production": {
-      "plugins": [
-        require('babel-plugin-transform-react-constant-elements'),
-        require('babel-plugin-transform-react-inline-elements'),
-        require('babel-plugin-transform-react-remove-prop-types')['default']
-      ]
-    },
-    "test": {
-      "plugins": [
-        require('babel-plugin-istanbul')
-      ]
-    }
-  }
+    require("babel-plugin-add-module-exports")
+  ]
 };
+
+var plugins = module.exports.plugins;
+if (env === 'test') {
+  plugins.push.apply(plugins, [
+    require('babel-plugin-istanbul')['default']
+  ]);
+}
+if (env === 'production') {
+  plugins.push.apply(plugins, [
+    // https://github.com/facebookincubator/create-react-app/issues/525
+    // require('babel-plugin-transform-react-constant-elements'),
+    require('babel-plugin-transform-react-inline-elements'),
+    require('babel-plugin-transform-react-remove-prop-types')['default']
+  ]);
+}
