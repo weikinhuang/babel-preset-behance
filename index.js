9c078911f029313f4e253cf4278c3bb080621ea3
Kai Cataldo <kaicataldo@gmail.com>
2016-10-04 17:21:52 -0400

    Add ability to specify target for included transforms

--- index.js
+++ index.js
@@ -1,27 +1,33 @@
-var env = process.env.BABEL_ENV || process.env.NODE_ENV;
+module.exports = function(context, opts) {
+  var env = process.env.BABEL_ENV || process.env.NODE_ENV;
+  opts = opts || {};
+  var browser = opts.browser !== false;
+  var config = {
+    presets: [
+      require('babel-preset-latest'),
+      require('babel-preset-stage-3')
+    ],
+    plugins: [
+      require('babel-plugin-add-module-exports')
+    ]
+  };
 
-module.exports = {
-  presets: [
-    require("babel-preset-latest"),
-    require("babel-preset-react"),
-    require("babel-preset-stage-3")
-  ],
-  plugins: [
-    require("babel-plugin-add-module-exports")
-  ]
+  if (env === 'test') {
+    config.plugins.push(require('babel-plugin-istanbul').default);
+  }
+
+  if (browser) {
+    config.presets.push(require('babel-preset-react'));
+
+    if (env === 'production') {
+      config.plugins.push.apply(config.plugins, [
+        // https://github.com/facebookincubator/create-react-app/issues/525
+        // require('babel-plugin-transform-react-constant-elements'),
+        require('babel-plugin-transform-react-inline-elements'),
+        require('babel-plugin-transform-react-remove-prop-types').default
+      ]);
+    }
+  }
+
+  return config;
 };
-
-var plugins = module.exports.plugins;
-if (env === 'test') {
-  plugins.push.apply(plugins, [
-    require('babel-plugin-istanbul')['default']
-  ]);
-}
-if (env === 'production') {
-  plugins.push.apply(plugins, [
-    // https://github.com/facebookincubator/create-react-app/issues/525
-    // require('babel-plugin-transform-react-constant-elements'),
-    require('babel-plugin-transform-react-inline-elements'),
-    require('babel-plugin-transform-react-remove-prop-types')['default']
-  ]);
-}
---
+++ test/index.js
@@ -0,0 +1,94 @@
+var preset = require('..');
+var expect = require('chai').expect;
+
+describe('babel-preset-behance', () => {
+  it('should default to "browser": true when no target is supplied', () => {
+    process.env.NODE_ENV = 'production';
+
+    const expected = {
+      presets: [
+        require('babel-preset-latest'),
+        require('babel-preset-stage-3'),
+        require('babel-preset-react')
+      ],
+      plugins: [
+        require('babel-plugin-add-module-exports'),
+        require('babel-plugin-transform-react-inline-elements'),
+        require('babel-plugin-transform-react-remove-prop-types').default
+      ]
+    };
+    expect(preset()).to.deep.equal(expected);
+  });
+
+  describe('when process env is "test"', () => {
+    it('should add babel-plugin-istanbul', () => {
+      process.env.NODE_ENV = 'test';
+
+      const expected = {
+        presets: [
+          require('babel-preset-latest'),
+          require('babel-preset-stage-3'),
+          require('babel-preset-react')
+        ],
+        plugins: [
+          require('babel-plugin-add-module-exports'),
+          require('babel-plugin-istanbul').default
+        ]
+      };
+      expect(preset()).to.deep.equal(expected);
+    });
+  });
+
+  describe('when "browser" is true in options', () => {
+    it('should add React preset but not the transforms when process env is not "production"', () => {
+      process.env.NODE_ENV = 'development';
+
+      const expected = {
+        presets: [
+          require('babel-preset-latest'),
+          require('babel-preset-stage-3'),
+          require('babel-preset-react')
+        ],
+        plugins: [
+          require('babel-plugin-add-module-exports')
+        ]
+      };
+      expect(preset(null, { browser: true })).to.deep.equal(expected);
+    });
+
+    it('should add React preset and transforms when process env is "production"', () => {
+      process.env.NODE_ENV = 'production';
+
+      const expected = {
+        presets: [
+          require('babel-preset-latest'),
+          require('babel-preset-stage-3'),
+          require('babel-preset-react')
+        ],
+        plugins: [
+          require('babel-plugin-add-module-exports'),
+          require('babel-plugin-transform-react-inline-elements'),
+          require('babel-plugin-transform-react-remove-prop-types').default
+        ]
+      };
+      expect(preset(null, { browser: true })).to.deep.equal(expected);
+    });
+  });
+
+  describe('when "browser" is false in options', () => {
+    it('should not add React preset or transforms', () => {
+      process.env.NODE_ENV = 'production';
+
+      const expected = {
+        presets: [
+          require('babel-preset-latest'),
+          require('babel-preset-stage-3'),
+        ],
+        plugins: [
+          require('babel-plugin-add-module-exports'),
+        ]
+      };
+      expect(preset(null, { browser: false })).to.deep.equal(expected);
+    });
+  });
+});
